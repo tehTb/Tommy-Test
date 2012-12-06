3787420716320ba22535e0246af0ffb2596ff8aa
tehTb <hallo_tommy@hotmail.com>
2012-12-06 03:39:10 +0100

    uf

---
+++ PirateSheep stuff/Prog/Test/test1/Project Space/src/com/mod/niklas/test/UglyFace.java
@@ -0,0 +1,21 @@
+package com.mod.niklas.test;
+
+import com.mod.niklas.engine.MODSimpleImage;
+import com.mod.niklas.engine.MODUpdateable;
+
+public class UglyFace implements MODUpdateable{
+
+	public MODSimpleImage img;
+	
+	public UglyFace()
+	{
+		
+	}
+	
+	@Override
+	public void update(long dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
